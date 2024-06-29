451d670f8506d45887797c28bd3dfe05b047eaa5
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-06-29 18:25:08 +0530

    Update Dashboard.tsx

--- src/Component/NewDashboard/Dashboard.tsx
+++ src/Component/NewDashboard/Dashboard.tsx
@@ -4,24 +4,25 @@ import styles from './Dashboard.module.css';
 import FullGrid from '../Grids/parent_grid/broker-grid';
 import axiosInstance from '../AxiosInterceptor/AxiosInterceptor';
 
-const [rows, setRows] = useState<[]>([]);
-const fetchDeals= async () => {
-    try {
-      const response = await axiosInstance.get('/deals');
-      const deals = response.data.map((deal: any) => ({
-        totalDeals: deal.totalDeals,
-        dealsOpened: deal.dealsOpened,
-        dealsInProgress: deal.dealsInProgress,
-        dealsClosed: deal.dealsClosed,
-        totalCommission: deal.totalCommission,
-      }));
-      setRows(deals);
 
-    } catch (error) {
-      console.error('Error fetching deals:', error);
-    }
-  };
 const Dashboard: React.FC = () => {
+    const [rows, setRows] = useState<[]>([]);
+    const fetchDeals= async () => {
+        try {
+          const response = await axiosInstance.get('/deals');
+          const deals = response.data.map((deal: any) => ({
+            totalDeals: deal.totalDeals,
+            dealsOpened: deal.dealsOpened,
+            dealsInProgress: deal.dealsInProgress,
+            dealsClosed: deal.dealsClosed,
+            totalCommission: deal.totalCommission,
+          }));
+          setRows(deals);
+    
+        } catch (error) {
+          console.error('Error fetching deals:', error);
+        }
+      };
     return (
         <div className={styles.dashboardContainer}>
             <Navbar />
