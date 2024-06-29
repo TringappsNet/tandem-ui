781a3072e2eaebb8f2f38019f523d500a9333b3f
DINESHKUMAR K V <dineshkumar.kv@tringapps.com>
2024-06-29 18:16:18 +0530

    Added Cards Component

--- src/Component/NewDashboard/Dashboard.tsx
+++ src/Component/NewDashboard/Dashboard.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
-// import Main from '../Main/Main'; // Import your Main component
-// import Cards from '../Cards/Cards'; // Import your Cards component
+// import Main from '../Main/Main'; // Make sure to import Main component
+import Cards from '../Cards/Cards';
 import styles from './Dashboard.module.css';
 
 const Dashboard: React.FC = () => {
-    return (
-        <Router>
-            <div className={styles.dashboardContainer}>
-                <Navbar />
-                <div className={styles.mainContent}>
-                    <Routes>
-                        <Route path="/dashboard">
-                            {/* <Main /> */}
-                        </Route>
-                        <Route path="/cards">
-                            {/* <Cards /> */}
-                        </Route>
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <div className={styles.dashboardContainer}>
+      <Navbar />
+      <div className={styles.mainContent}>
+        <Routes>
+          {/* <Route path="/" element={<Main />} /> */}
+          <Route path="/cards" element={<Cards />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
