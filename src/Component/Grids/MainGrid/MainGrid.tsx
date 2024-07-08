ad8c61e6025be4ffa59eb33d9af563c4990dba83
Siva-bharathiP <136304492+Siva-bharathiP@users.noreply.github.com>
2024-07-08 12:55:31 +0530

    Uialignment

--- src/Component/Grids/MainGrid/MainGrid.tsx
+++ src/Component/Grids/MainGrid/MainGrid.tsx
@@ -52,6 +52,24 @@ const FullGrid: React.FC<FullGridProps> = ({
                 border: 0,
                 ...sx,
                 lineHeight:2,
+'.css-15n4jlm-MuiDataGrid-root':
+{
+ border:'none',
+},
+  '.css-ptiqhd-MuiSvgIcon-root ':{
+  marginLeft:'10px',
+
+},
+                '.css-1eed5fa-MuiInputBase-root-MuiInput-root': {
+
+                  border: '1px solid #d4d4d4',
+                  borderRadius: '4px',
+                  width:'500px',
+                  marginRight:'840px',
+                  padding:'2px',
+                  marginTop:'10px',
+                  marginBottom:'10px'
+              },
                 '.css-1pe4mpk-MuiButtonBase-root-MuiIconButton-root':{
                      color:'white',
                      '&:hover':{
@@ -97,6 +115,7 @@ const FullGrid: React.FC<FullGridProps> = ({
 
                 '.css-1knaqv7-MuiButtonBase-root-MuiButton-root':{
                   visibility:'hidden',
+                  position:'absolute',
                   marginBottom:2,
                   
                 },
