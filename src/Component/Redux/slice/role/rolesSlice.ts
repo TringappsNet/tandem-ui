a830626340841eda006e673128fc85591f00ca91
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-07-10 14:16:10 +0530

    Update rolesSlice.ts

--- src/Component/Redux/slice/role/rolesSlice.ts
+++ src/Component/Redux/slice/role/rolesSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axiosInstance from '../../../AxiosInterceptor/AxiosInterceptor';
 import { axiosInstance } from '../../../AxiosInterceptor/AxiosInterceptor';
 import { RootState } from '../../reducers';
 
