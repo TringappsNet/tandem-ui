94c9fb39c7fb88cce8dd7731425fb4cc6ee633b8
DharshanRam Kumar K <dharshanramk@gmail.com>
2024-07-12 19:44:23 +0530

    update

--- src/Component/Grids/MainGrid/MainGrid.tsx
+++ src/Component/Grids/MainGrid/MainGrid.tsx
@@ -47,14 +47,9 @@ const FullGrid: React.FC<FullGridProps> = ({
           sx={{
             height: 800,
             marginTop: 2,
-<<<<<<< UI-Theme-Update
             bgcolor: '#142031 !important', // Set the background color here with !important
               // border:'1px solid white',
 
-=======
-            bgcolor: 'darkblue !important', // Set the background color here with !important
-            border: '#191c24 !important',
->>>>>>> main
 
             ...sx,
 
@@ -62,7 +57,6 @@ const FullGrid: React.FC<FullGridProps> = ({
             '.css-15n4jlm-MuiDataGrid-root': {
               border: 'none',
             },
-<<<<<<< UI-Theme-Update
             '.css-1kwdphh-MuiDataGrid-virtualScrollerContent,.css-tgsonj,.css-14mxsc7-MuiDataGrid-root .MuiDataGrid-withBorderColor,.css-1rtad1,.css-14mxsc7-MuiDataGrid-root .MuiDataGrid-filler,.css-128fb87-MuiDataGrid-toolbarContainer,.css-wop1k0-MuiDataGrid-footerContainer, .css-yrdy0g-MuiDataGrid-columnHeaderRow,.css-1w53k9d-MuiDataGrid-overlay,.css-1rtad1,.css-og3pwy .css-15n4jlm-MuiDataGrid-root':{
               background: '#142031 !important',
               color: 'white',
@@ -84,23 +78,6 @@ const FullGrid: React.FC<FullGridProps> = ({
           }
           
 
-=======
-            '.css-1kwdphh-MuiDataGrid-virtualScrollerContent,.css-tgsonj,.css-14mxsc7-MuiDataGrid-root .MuiDataGrid-withBorderColor,.css-1rtad1,.css-14mxsc7-MuiDataGrid-root .MuiDataGrid-filler,.css-128fb87-MuiDataGrid-toolbarContainer,.css-wop1k0-MuiDataGrid-footerContainer, .css-yrdy0g-MuiDataGrid-columnHeaderRow,.css-1w53k9d-MuiDataGrid-overlay,.css-1rtad1':
-              {
-                background: '#191c24 !important',
-                color: 'white',
-                border: '#191c24',
-              },
-            '.css-1eed5fa-MuiInputBase-root-MuiInput-root': {
-              color: 'white',
-            },
-            '.css-1pe4mpk-MuiButtonBase-root-MuiIconButton-root': {
-              color: 'white',
-            },
-            '.css-14mxsc7-MuiDataGrid-root ': {
-              border: 'none',
-            },
->>>>>>> main
           }}
         >
           <DataGrid
