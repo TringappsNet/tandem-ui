29726b1d25d4214eead7b056d23ed97f66e2186e
Dhaya1599 <129719964+Dhaya1599@users.noreply.github.com>
2024-07-15 11:08:25 +0530

    updated file of cards

--- src/Component/Cards/Cards.tsx
+++ src/Component/Cards/Cards.tsx
@@ -14,25 +14,6 @@ import ConfirmationModal from '../AlertDialog/AlertDialog';
 import { setCurrentDeal } from '../Redux/slice/deal/currentDeal';
 import { Deal } from '../Interface/DealFormObject';
 
-// interface Deal {
-//   id: number | null;
-//   brokerName: string;
-//   propertyName: string;
-//   dealStartDate: string;
-//   proposalDate: string;
-//   loiExecuteDate: string;
-//   leaseSignedDate: string;
-//   noticeToProceedDate: string;
-//   commercialOperationDate: string;
-//   potentialcommissiondate: string;
-//   potentialCommission: number | 0;
-//   status: string;
-//   activeStep: number;
-//   createdBy: number;
-//   updatedBy: number;
-//   isNew: boolean;
-//   updatedAt?: string;
-// }
 
 const Cards: React.FC = () => {
   const userdetails = useSelector((state: RootState) => state.auth);
