019cc7dc82988a5766f075e8f22c52083916cd0d
DINESHKUMAR K V <dineshkumar.kv@tringapps.com>
2024-07-19 16:46:03 +0530

    Added Progress for Broker Login

--- src/Component/AxiosInterceptor/AxiosInterceptor.tsx
+++ src/Component/AxiosInterceptor/AxiosInterceptor.tsx
@@ -6,7 +6,7 @@ import styles from './AxiosInterceptor.module.css';
 
 const axiosInstance = axios.create({
   //baseURL: "http://localhost:3008/api",
-  baseURL: 'http://192.168.1.223:3008/api', //tharun 
+  baseURL: 'http://192.168.1.47:3008/api', //tharun 
 });
 
 const getQueryParam = (param: string): string | null => {
