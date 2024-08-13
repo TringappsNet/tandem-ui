24e5c8ff53c5d1152ebcc8e226ff2a3a837e910f
DINESHKUMAR K V <dineshkumar.kv@tringapps.com>
2024-08-13 15:28:09 +0530

    Updated Register password & build file

--- src/Component/Registration/Registration.tsx
+++ src/Component/Registration/Registration.tsx
@@ -88,7 +88,7 @@ const Registration: React.FC = () => {
     if (password.length >= 8) strength += 1;
     if (/[A-Z]/.test(password)) strength += 1;
     if (/[a-z]/.test(password)) strength += 1;
-    if (/[0-9]/.test(password)) strength += 1;
+    if (/\d{2,}/.test(password)) strength += 1;
     if (/[^A-Za-z0-9]/.test(password)) strength += 1;
     setStrength(strength);
   };
